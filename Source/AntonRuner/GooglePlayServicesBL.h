ba7e43996d3e5434b44b0d18123c8323a96c3343
Mioryy <sterhovnikita@gmail.com>
2020-12-17 18:08:10 +0300

    Update

---
+++ Source/AntonRuner/GooglePlayServicesBL.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "GooglePlayServicesBL.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANTONRUNER_API UGooglePlayServicesBL : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
